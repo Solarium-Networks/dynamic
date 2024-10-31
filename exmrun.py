77c61985d150ac5b717478e6494648b361d5882c
T_cat9000_2 <terpstragraham@gmail.com>
2024-10-30 19:42:39 -0700

    clean uo and advance code

--- exmrun.py
+++ exmrun.py
@@ -1,10 +1,4 @@
-# import module
 import exm
 
-# module running process:
-# make module name
-module_name = "exm"
-# get module name from globals
-module = globals().get(module_name)
-# run
-module.example("world")
+exm.example("world")
+exm.mode("mode? MODE? nAH!!!")
