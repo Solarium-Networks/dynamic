2018baf7b993cded298f607e2cb7cad87eaeb3c7
T_cat9000_2 <terpstragraham@gmail.com>
2024-10-30 19:46:39 -0700

    clean up and add MODE function

--- exmrun.py
+++ exmrun.py
@@ -1,4 +1,9 @@
 import exm
 
-exm.example("world")
+exm.example("world") 
 exm.mode("mode? MODE? nAH!!!")
+
+"""
+look here: we import the FILE NAME WITHOUT THE .py EXTENSION, and THEN use the module! KEWL! right?
+so yes, if we ever make them this is sort of how we can make dynamic python librarys KEWL! right?
+"""
