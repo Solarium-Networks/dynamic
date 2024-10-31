8706b1ab6db45dc1e2920aba83d8aacca8707218
T_cat9000_2 <terpstragraham@gmail.com>
2024-10-30 19:40:01 -0700

    add mode

--- exm.py
+++ exm.py
@@ -1,2 +1,5 @@
 def example(param):
   print(f"hello, {param}")
+# see how this is kinda like a library defining these functions and using them in exmrun.py? lets make some more!
+def mode(mode):
+  print(f"the machine state mode is: {mode}")
